cf37537b00fb524aa7795eecf1d8da065aca126f
Subhra Sankha Saha <jeesan2006@gmail.com>
2024-08-12 21:00:29 +0530

    Variable Declaration in Go

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/suv2013/learning-go-2875237
+
+go 1.22.6
--- practice/main.go
+++ practice/main.go
@@ -5,5 +5,19 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello from Go!")
+	varDeclaration()
+}
+
+func varDeclaration() {
+	var str = "Hello from Go!"
+	fmt.Println(str)
+	fmt.Printf("%T \n", str)
+
+	var inte = 42
+	fmt.Println(inte)
+	fmt.Printf("%T \n", inte)
+
+	strNew := "Hello from Go!"
+	fmt.Println(strNew)
+	fmt.Printf("%T \n", strNew)
 }
